22163206186d43516ac948697675f8b93c6e8f04
Charly Bernabe <charly.ivan.bernabe@gmail.com>
2021-02-24 11:49:05 -0600

    port

--- src/main.ts
+++ src/main.ts
@@ -21,6 +21,6 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
   app.enableCors();
-  await app.listen(3100);
+  await app.listen(3000);
 }
 bootstrap();
